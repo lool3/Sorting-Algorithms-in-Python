9852e9bfd96710155909bd0adf8c13419325e311
Joe Wood <38761945+lool3@users.noreply.github.com>
2023-03-06 20:06:25 +0000

    Cleanup and bugfix

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
 import sortmylist
 import random, time
-
-
 def main():
   print(
     "This program will sort a random list of integers from smallest to largest"
   )
   randomList = list(range(1000))  #Create array of random integers to sort
-  random.shuffle(randomList) #Shuffles the list
-    
+  random.shuffle(randomList)
   algorithms = {
-    0:sortmylist.bubble(randomList),
-    1:sortmylist.selection(randomList),
-    2:sortmylist.insertion(randomList),
-    3:sortmylist.quick(randomList),
-    4:sortmylist.merge(randomList),
-    5:sortmylist.counting(randomList),
-    6:sortmylist.radix(randomList),
-    7:sortmylist.bucket(randomList),
-    8:sortmylist.comb(randomList)
+    0: sortmylist.bubble,
+    1: sortmylist.selection,
+    2: sortmylist.insertion,
+    3: sortmylist.quick,
+    4: sortmylist.merge,
+    5: sortmylist.counting,
+    6: sortmylist.radix,
+    7: sortmylist.bucket,
+    8: sortmylist.comb
   }
   algorithm = choose()
-  
   start = time.time()
-  print(algorithms.get(algorithm))
+  print(algorithms.get(algorithm)(randomList))
   print(f'Time taken in seconds: {time.time() - start}')
-
-
 def choose():
   algorithms = ("BUBBLE", "SELECTION", "INSERTION", "QUICK", "MERGE",
                 "COUNTING", "RADIX", "BUCKET", "COMB")
   for i, item in enumerate(algorithms):
     print(f"{i}. {item}")
-
   while True:
     try:
       choice = int(input("Pick a sorting algorithm: "))
@@ -43,7 +36,5 @@ def choose():
     except ValueError as err:
       print(f"Error: {err}. Try again...")
       continue
-
-
 if __name__ == "__main__":
   main()
