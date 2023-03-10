433d832e25df8caef6ae065d438dffaf57d39129
Joe Wood <38761945+lool3@users.noreply.github.com>
2023-03-10 14:08:26 +0000

    Minor logic error fix

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import random, time
 def main():
   randy = [random.randrange(1, 11) for x in range(10)]
   result = menu.menu({
-    "Bubble sort": sortmylist.bubble(randy),
-    "Selection sort": sortmylist.selection(randy),
-    "Insertion sort": sortmylist.insertion(randy),
-    "Quick sort": sortmylist.quick(randy)
+    "Bubble sort": sortmylist.bubble,
+    "Selection sort": sortmylist.selection,
+    "Insertion sort": sortmylist.insertion,
+    "Quick sort": sortmylist.quick
   })
-  print(result)
+  print(result(randy))
 
 main()
