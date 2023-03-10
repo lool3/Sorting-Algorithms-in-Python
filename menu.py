0096277f4a6fb2960301d359297e33a080e6a1f1
Joe Wood <38761945+lool3@users.noreply.github.com>
2023-03-10 14:17:25 +0000

    Added exit option

--- menu.py
+++ menu.py
@@ -3,15 +3,18 @@ def menu(menuOptions):
     print("----------Main-Menu----------\n")
     for i, option in enumerate(menuOptions.keys()):
       print(f"{i+1}. {option}")
-
+    print(f"{len(menuOptions) + 1}. Exit")
+    print("\n-----------------------------\n")
     try:
-      menuChoice = int(input("\nChoice: "))
+      menuChoice = int(input("Choice: "))
     except ValueError:
       print("That's not even a number!")
       continue
     else:
-      if menuChoice not in range(1, len(menuOptions) + 1):
+      if menuChoice == len(menuOptions) + 1:
+        exit("Goodbye!")
+      elif menuChoice not in range(1, len(menuOptions) + 1):
         print("That's not an option!")
         continue
-
+    
     return list(menuOptions.values())[menuChoice - 1]
